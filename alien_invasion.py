c364c2a5cec8ed9764d3ae2125481e8300797f68
Bkm79 <bkm79@yandex.ru>
2019-03-06 15:45:53 +0300

    change background color

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,17 +3,21 @@ import sys
 import pygame
 
 def run_game():
-    """инициализация и создание объекта экрана"""
+    #инициализация и создание объекта экрана
     pygame.init()
     screen = pygame.display.set_mode((1200,800))
     pygame.display.set_caption("Alien Invasion")
+    bg_color = (230, 230, 230)
     
-    """запуск основного цикла"""
+    #запуск основного цикла
     while True:
-        #отслеживание событий клавы и мыши
+        #отслеживание событий клавиатуры и мыши
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        
+        #перерисовка экрана
+        screen.fill(bg_color)
         
         #отображение последего отрисованного экрана
         pygame.display.flip()
