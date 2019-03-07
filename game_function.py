04837657e553f67d857ad0208153f31e4000e1ea
Bkm79 <bkm79@yandex.ru>
2019-03-07 12:06:49 +0300

    ship moving add

--- game_function.py
+++ game_function.py
@@ -2,12 +2,23 @@ import sys
 
 import pygame
 
-def check_events():
+def check_events(ship):
     """track keyboard event"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
+        
+                
 def update_screen(ai_settings, screen, ship):
     """update screen"""
     screen.fill(ai_settings.bg_color)
